8faaf5c2a6e0f2aabbf07677590ca76df3694db7
Michael Chow <machow@princeton.edu>
2016-05-12 17:38:16 -0400

    update pars for power sim

--- vignettes/0_power_analysis/0_create_sim_pars.R
+++ vignettes/0_power_analysis/0_create_sim_pars.R
@@ -2,8 +2,9 @@
 OUTNAME <- if (!interactive()) commandArgs(TRUE)[1] else 'test_pars.csv'
 
 # Create parameters
-pars = expand.grid(Nobs=400, nsubs1=10, nsubs2=c(5, 8, 22), 
-                   lam1=c(.45,.65,.85), lam2=c(.45, .65,.85), rho=c(1))
+loadings <- c(.25, .3, .45, .65, .85)
+pars = expand.grid(Nobs=c(100, 400), nsubs1=10, nsubs2=c(5, 10, 22), 
+                   lam1=loadings, lam2=loadings, rho=c(1))
 pars = subset(pars, lam1 >= lam2)
 
 write.csv(pars, file = OUTNAME, row.names=FALSE)
